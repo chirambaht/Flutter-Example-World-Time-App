3a0d3a319e4c1ad7cc125271111141191d42f2ad
Humphrey Chiramba <chirambaht@gmail.com>
2021-01-18 23:30:26 +0200

    Complete work done. Now a rookie!

--- lib/screens/choose_location.dart
+++ lib/screens/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ninja_app/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +7,61 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(
+        url: 'Europe/London', location: 'London', flag: 'assets/images/uk.png'),
+    WorldTime(
+        url: 'Europe/Berlin',
+        location: 'Athens',
+        flag: 'assets/images/greece.png'),
+    WorldTime(
+        url: 'Africa/Cairo',
+        location: 'Cairo',
+        flag: 'assets/images/egypt.png'),
+    WorldTime(
+        url: 'Africa/Nairobi',
+        location: 'Nairobi',
+        flag: 'assets/images/kenya.png'),
+    WorldTime(
+        url: 'America/Chicago',
+        location: 'Chicago',
+        flag: 'assets/images/usa.png'),
+    WorldTime(
+        url: 'America/New_York',
+        location: 'New York',
+        flag: 'assets/images/usa.png'),
+    WorldTime(
+        url: 'Asia/Seoul',
+        location: 'Seoul',
+        flag: 'assets/images/south_korea.png'),
+    WorldTime(
+        url: 'Asia/Jakarta',
+        location: 'Jakarta',
+        flag: 'assets/images/indonesia.png'),
+    WorldTime(
+        url: 'Africa/Johannesburg',
+        location: 'Johannesburg',
+        flag: 'assets/images/sa.png'),
+    WorldTime(
+        url: 'Africa/Johannesburg',
+        location: 'Harare',
+        flag: 'assets/images/zim.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+
+    await instance.getTime();
+
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+      'url': instance.url
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +71,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         title: Text('Choose a location'),
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 1),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  updateTime(index);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(locations[index].flag),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context).settings.arguments;
+    data = (data.isNotEmpty) ? data : ModalRoute.of(context).settings.arguments;
 
     print(data);
 
@@ -33,8 +33,17 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'isDaytime': result['isDaytime'],
+                        'flag': result['flag']
+                      };
+                    });
                   },
                   icon: Icon(
                     Icons.edit_location,
@@ -50,6 +59,13 @@ class _HomeState extends State<Home> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Image.asset(
+                    (data['flag']),
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     '${data['location']}',
                     style: TextStyle(
