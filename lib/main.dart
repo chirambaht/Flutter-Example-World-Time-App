1aa7c73fe6e453fdc74f60f50c7510bd64bb798d
Humphrey Chiramba <chirambaht@gmail.com>
2021-01-18 10:54:25 +0200

    Listy lessons, don't forget arrow functions

--- lib/main.dart
+++ lib/main.dart
@@ -2,106 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: QuoteList(),
   ));
 }
 
-class Home extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
-  int ninjaLevel = 0;
-
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'To be or not to be, that is the question',
+    'There is no one better that you at being you, don\'t mess it up',
+    'Dogs also think, don\' discredit yourself so quickly'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey[800],
-        title: Text('Ninja ID'),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: Colors.grey[900],
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 0.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 30.0,
-              ),
-              Center(
-                child: CircleAvatar(
-                  radius: 40.0,
-                  backgroundImage: AssetImage('assets/images/alex.jpg'),
-                ),
-              ),
-              Divider(
-                height: 40.0,
-                color: Colors.blue,
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(fontSize: 15.0, color: Colors.grey[350]),
-              ),
-              Text(
-                'Chun-Li',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.yellow,
-                    fontSize: 35.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'CURRENT NINJA LEVEL',
-                style: TextStyle(fontSize: 15.0, color: Colors.grey[350]),
-              ),
-              Text(
-                '$ninjaLevel',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.yellow,
-                    fontSize: 35.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[300],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'chun-li@ninja.com',
-                  style: TextStyle(
-                    color: Colors.grey[300],
-                  ),
-                )
-              ]),
-            ],
-          ),
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesone Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel++;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.yellow,
-      ),
-    );
+        body: Column(
+          children: quotes.map((quote) {
+            return Text(quote);
+          }).toList(),
+        ));
   }
 }
