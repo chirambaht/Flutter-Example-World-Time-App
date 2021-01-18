20c367721e1c6871081c7e2b1f3ca5849e902318
Humphrey Chiramba <chirambaht@gmail.com>
2021-01-18 12:56:59 +0200

    Base work done here for the time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:ninja_app/quote.dart';
-
-import 'quote_card.dart';
+import 'package:ninja_app/screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    home: Home(),
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(text: 'To be or not to be, that is the question', author: 'Hamlet'),
-    Quote(
-        text: 'There is no one better that you at being you, don\'t mess it up',
-        author: 'Humphrey'),
-    Quote(
-        text: 'Dogs also think, don\' discredit yourself so quickly',
-        author: 'Dirty Bag')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text('Awesone Quotes'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Column(
-          children: quotes.map((quote) {
-            return QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                });
-          }).toList(),
-        ));
-  }
 }
